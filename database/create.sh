6cdc7c5938dd248744d28df8ce89afdf067cece9
y c <40169115+extreme4all@users.noreply.github.com>
2022-07-10 20:35:50 +0200

    restructuring

--- database/create.sh
+++ database/create.sh
@@ -1,7 +1,15 @@
-sudo mysql -u root < /home/ubuntu/recreate_playerdata_dev.sql
+#!/bin/sh
+
+sudo mysql -u root </home/ubuntu/000_recreate_playerdata_dev.sql
 sudo mysqldump -u root --no-data --routines --events playerdata | sudo mysql -u root playerdata_dev
-sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerdata_dev.sql
-sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerReports.sql
-sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerReportsManual.sql
-sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerLocationsDetail.sql
-sudo mysqldump -u root --routines --events playerdata_dev | gzip > /home/ubuntu/playerdata_dev.sql
+sudo mysql -u root playerdata_dev </home/ubuntu/001_insert_apiUser.sql
+sudo mysql -u root playerdata_dev </home/ubuntu/002_insert_players.sql
+sudo mysql -u root playerdata_dev </home/ubuntu/003_insert_playerdata_dev.sql
+sudo mysql -u root playerdata_dev </home/ubuntu/004_insert_playerHiscoreData.sql
+sudo mysql -u root playerdata_dev </home/ubuntu/005_insert_reports.sql
+
+sudo mysqldump -u root --routines --events playerdata_dev | gzip >/home/ubuntu/playerdata_dev.sql
+
+# sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerReports.sql
+# sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerReportsManual.sql
+# sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerLocationsDetail.sql
--- database/recreate_playerdata_dev.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS playerdata_dev;
-CREATE DATABASE playerdata_dev;
