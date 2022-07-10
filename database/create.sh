3a7595a78eed2c81d78f1fe12c0773c89fc03b2e
y c <40169115+extreme4all@users.noreply.github.com>
2022-07-11 00:19:15 +0200

    Update create.sh

--- database/create.sh
+++ database/create.sh
@@ -7,8 +7,7 @@ sudo mysql -u root playerdata_dev </home/ubuntu/002_insert_players.sql
 sudo mysql -u root playerdata_dev </home/ubuntu/003_insert_playerdata_dev.sql
 sudo mysql -u root playerdata_dev </home/ubuntu/004_insert_playerHiscoreData.sql
 sudo mysql -u root playerdata_dev </home/ubuntu/005_insert_reports.sql
-
-sudo mysqldump -u root --routines --events playerdata_dev | gzip >/home/ubuntu/playerdata_dev.sql
+sudo mysqldump -u root --routines --events --databases playerdata_dev | sed 's/playerdata_dev/playerdata/g'|  gzip >/home/ubuntu/100_playerdata_dev.sql.gz
 
 # sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerReports.sql
 # sudo mysql -u root playerdata_dev < /home/ubuntu/insert_playerReportsManual.sql
